6fa4d3609bddd76b9b6d5d35f289de93d8fb0897
khabbab mahmud <onlykhabbabmahmud@gmail.com>
2022-04-27 12:58:40 +0600

    some change

--- 4_laravel_ajax_email_paypal/resources/views/welcome.blade.php
+++ 4_laravel_ajax_email_paypal/resources/views/welcome.blade.php
@@ -40,50 +40,25 @@
         // Sets up the transaction when a payment button is clicked
         createOrder: function(data, actions) {
 
-return actions.order.create({
-
-  intent: 'CAPTURE',
-
-  purchase_units: [{
-
-    amount: {
-
-      currency_code: 'USD',
-
-      value: '220.00'
-
-    },
-
-    payee: {
-
-      email_address: 'receiveraccount@emaildomain.com'
-
-    }
-
-  }]
-
-});
-
-},
+    return actions.order.create({
+        intent: 'CAPTURE',
+        purchase_units: [{
+            amount: {
+            currency_code: 'USD',
+            value: '00.02'
+            },
+            payee: {
+            email_address: 'sb-enyix15178627@business.example.com'
+            }
+        }]
+        });
+        },
 
         // Finalize the transaction after payer approval
         onApprove: (data, actions) => {
           return actions.order.capture().then(function(orderData) {
 
-            // Successful capture! For dev/demo purposes:
-
-            console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
-
-            const transaction = orderData.purchase_units[0].payments.captures[0];
-
-            alert(`Transaction ${transaction.status}: ${transaction.id}\n\nSee console for all available details`);
-
-            // When ready to go live, remove the alert and show a success message within this page. For example:
-
-            // element.innerHTML = '<h3>Thank you for your payment!</h3>';
-
-            // Or go to another URL:  actions.redirect('thank_you.html');
-
+           
           });
         }
       }).render('#paypal-button-container');
